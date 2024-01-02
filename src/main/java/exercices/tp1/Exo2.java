70139439b9c0764d2ce3683e8c80e984b8bca9b4
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-01-02 16:44:18 +0100

    Ajout méthode verifyIfNumberIsPositive() et calculateTtcPrice()

---
+++ src/main/java/exercices/tp1/Exo2.java
@@ -0,0 +1,27 @@
+package exercices.tp1;
+
+public class Exo2 {
+    public static final float TVA = 20;
+
+    /**
+     * Méthode privée qui vérifie si un nombre est positif
+     * @param number float
+     * @throws IllegalArgumentException exception
+     */
+    private static void verifyIfNumberIsPositive(float number) throws IllegalArgumentException {
+        if (number <= 0) {
+            throw new IllegalArgumentException("Le nombre doit être positif");
+        }
+    }
+
+    /**
+     * Calcule le prix TTC
+     * @param number float
+     * @return float - le prix TTC d'un paramètre number
+     */
+    public static float calculateTtcPrice(float number) {
+        verifyIfNumberIsPositive(number);
+
+        return number*(1+(TVA/100));
+    }
+}
