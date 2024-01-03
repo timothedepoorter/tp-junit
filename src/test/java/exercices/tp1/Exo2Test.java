531e22c9a57c8adb128156c4c4ea987ec4033f72
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-01-03 09:14:20 +0100

    modification import assertion * pour assertEquals

--- src/test/java/exercices/tp1/Exo2Test.java
+++ src/test/java/exercices/tp1/Exo2Test.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Exo2Test {
     Exo2 exo2;
