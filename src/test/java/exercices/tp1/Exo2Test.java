05ca19ad0cf538aa69aaba906ae7ba38f4939060
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-01-03 09:13:06 +0100

    Test de la classe Exo2

---
+++ src/test/java/exercices/tp1/Exo2Test.java
@@ -0,0 +1,31 @@
+package exercices.tp1;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Exo2Test {
+    Exo2 exo2;
+    static float TVA;
+
+    @BeforeAll
+    public static void initAll() {
+        TVA = 20;
+    }
+
+    @BeforeEach
+    public void initEach() {
+        exo2 = new Exo2();
+    }
+
+    @ParameterizedTest(name = "Test {index} : {0}")
+    @ValueSource(floats = {100})
+    @DisplayName("Calcule prix TTC")
+    public void testCalculateTtcPrice(float price) {
+        assertEquals(120, Math.round(exo2.calculateTtcPrice(price)));
+    }
+}
