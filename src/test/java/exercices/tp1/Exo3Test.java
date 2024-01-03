b049a542700cee8f40acc1de7af8e62d59c244ae
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-01-03 11:32:46 +0100

    Ajout test pour IllegalArgumentException

--- src/test/java/exercices/tp1/Exo3Test.java
+++ src/test/java/exercices/tp1/Exo3Test.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class Exo3Test {
     Exo3 exo3;
@@ -22,5 +24,19 @@ class Exo3Test {
     @DisplayName("Test de la méthode isPalindrome()")
     public void testIsPalindrome(int numberToTest, boolean expected) {
         assertEquals(expected, exo3.isPalindrome(numberToTest));
+    }
+
+    @ParameterizedTest(name = "Test {index} : {0}")
+    @ValueSource(ints = {-11011})
+    @DisplayName("Test de la méthode isPalindrome() avec un nombre négatif")
+    public void testIsPalindromeWithNegativeNumber(int negativeNumber) {
+        assertThrows(IllegalArgumentException.class, () -> exo3.isPalindrome(negativeNumber));
+    }
+
+    @ParameterizedTest(name = "Test {index} : {0}")
+    @ValueSource(ints = {5})
+    @DisplayName("Test de la méthode isPalindrome() avec un nombre composé d'un seul caractère")
+    public void testIsPalindromeWithOneCharacterNumber(int oneCharacterNumber) {
+        assertThrows(IllegalArgumentException.class, () -> exo3.isPalindrome(oneCharacterNumber));
     }
 }
