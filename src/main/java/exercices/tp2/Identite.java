2a4b4e3c7ee8aa4d3f5895405a5bc843b0083f68
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-01-04 11:26:50 +0100

    Classe Identite

---
+++ src/main/java/exercices/tp2/Identite.java
@@ -0,0 +1,38 @@
+package exercices.tp2;
+
+public class Identite {
+
+    private String nom;
+    private String prenom;
+    private Adresse adresse;
+
+    public Identite(String nom, String prenom, Adresse adresse) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+}
