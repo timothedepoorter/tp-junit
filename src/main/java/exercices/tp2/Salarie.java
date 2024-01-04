589cbfc2a29c70d10f69f851b265377ad1688625
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-01-04 16:10:07 +0100

    Ajout description aux méthodes

--- src/main/java/exercices/tp2/Salarie.java
+++ src/main/java/exercices/tp2/Salarie.java
@@ -25,10 +25,18 @@ public abstract class Salarie {
         this.identite = identite;
     }
 
+    /**
+     * Méthode permettant de modifier l'adresse du salarié
+     * @param newAdresse Adresse
+     */
     public void demenager(Adresse newAdresse) {
         this.identite.setAdresse(newAdresse);
     }
 
+    /**
+     * Méthode affichant en console une description du salaire du salarié selon son temps de travail passé en paramètre.
+     * @param heures int
+     */
     public void travailler(int heures) {
         String phraseNom = "Le salarié " + this.identite.getPrenom() + " " + this.identite.getNom() + " ";
         String phraseHeuresTravail = heures + " heures ";
@@ -36,10 +44,18 @@ public abstract class Salarie {
         System.out.println(phraseNom + phraseHeuresTravail + phraseSalaire);
     }
 
+    /**
+     * Surcharge de la méthode travailler(int heures) sans aucun paramètre.
+     * Sans paramètre, la méthode s'exécute avec comme valeur par défaut pour heures : 151.
+     */
     public void travailler() {
         this.travailler(151);
     }
 
+    /**
+     * Méthode permettant d'augmenter le salaire du salarié selon un pourcentage passé en paramètre.
+     * @param pourcentage double
+     */
     public void augmenter(double pourcentage) {
         this.salaire.setTauxHoraire(this.salaire.getTauxHoraire() * (1 + (pourcentage/100.0)));
     }
