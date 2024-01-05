b25c5dae3b535b4031eb8ddc959d2e5db60a5dd5
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-01-05 14:53:32 +0100

    Cas où aucune température : test invalide ❌

--- src/test/java/exercices/tp3/ThermometreTest.java
+++ src/test/java/exercices/tp3/ThermometreTest.java
@@ -1,4 +1,15 @@
 package exercices.tp3;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ThermometreTest {
+
+    @Test
+    public void should_return_0_when_array_empty() {
+        Thermometre thermometre = new Thermometre();
+        int[] temperatures = {};
+        assertEquals(0, thermometre.donneTemperature(temperatures));
+    }
 }
