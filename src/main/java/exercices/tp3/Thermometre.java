13ff7e400a427dd37a352023da6dc8772af91cba
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-01-05 16:31:13 +0100

    Refactorisation de la classe Thermometre

--- src/main/java/exercices/tp3/Thermometre.java
+++ src/main/java/exercices/tp3/Thermometre.java
@@ -1,21 +1,28 @@
 package exercices.tp3;
 
 public class Thermometre {
-    public int donneTemperature(int[] temperatures) {
+
+    private void siTropDeTemperatures(int[] temperatures) throws IllegalArgumentException {
         if (temperatures.length > 10000) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Vous ne pouvez pas entrer plus de 10 000 températures.");
         }
+    }
+
+    public int donneTemperature(int[] temperatures) {
+        siTropDeTemperatures(temperatures);
+        int plusProcheDeZero;
         if (temperatures.length == 0) {
-            return 0;
-        }
-        int plusProcheDeZero = temperatures[0];
-        for (int temperature : temperatures) {
-            if (Math.abs(temperature) < Math.abs(plusProcheDeZero)) {
-                plusProcheDeZero = temperature;
-            }
-            if (plusProcheDeZero < 0) {
-                if (Math.abs(plusProcheDeZero) == Math.abs(temperature)) {
+            plusProcheDeZero = 0;
+        } else {
+            plusProcheDeZero = temperatures[0];
+            for (int temperature : temperatures) {
+                if (Math.abs(temperature) < Math.abs(plusProcheDeZero)) {
                     plusProcheDeZero = temperature;
+                }
+                if (plusProcheDeZero < 0) {
+                    if (Math.abs(plusProcheDeZero) == Math.abs(temperature)) {
+                        plusProcheDeZero = temperature;
+                    }
                 }
             }
         }
