6c0d33502b5941dc43396c7d5880b1c321ef2841
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-01-05 15:32:09 +0100

    Cas où températures = {-2, -8, 4, 5} : test valide ✅

--- src/test/java/exercices/tp3/Thermometre.java
+++ src/test/java/exercices/tp3/Thermometre.java
@@ -5,6 +5,12 @@ public class Thermometre {
         if (temperatures.length == 0) {
             return 0;
         }
-        return 1;
+        int plusProcheDeZero = temperatures[0];
+        for (int temperature : temperatures) {
+            if (Math.abs(temperature) < Math.abs(plusProcheDeZero)) {
+                plusProcheDeZero = temperature;
+            }
+        }
+        return plusProcheDeZero;
     }
 }
